1368e4501bc2751d9ff96cca16d428cb4e27017b
chanjongpak <chanjong.pak@gmail.com>
2023-05-11 19:02:49 -0400

    add temp details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
+import TempAndDetails from "./components/TempAndDetails";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Inputs />
 
       <TimeAndLocation />
+      <TempAndDetails />
     </div>
   );
 }
