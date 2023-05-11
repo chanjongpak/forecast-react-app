a63900c2a8443efc17b462353d7c33119dd5c3b1
chanjongpak <chanjong.pak@gmail.com>
2023-05-11 18:40:15 -0400

    add time and location line

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">Lux Numquam Decit</p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Amsterdam</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
