6bc6fff4c6c7463378a61935b4212d425ae0fa66
chanjongpak <chanjong.pak@gmail.com>
2023-05-12 22:28:07 -0400

    add services

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
+import getWeatherData from "./services/weatherService";
 
 function App() {
+  const fetchWeather = async () => {
+    const data = await getWeatherData("weather", { q: "amsterdam" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
---
+++ src/services/weatherService.js
@@ -0,0 +1,45 @@
+const API_KEY = "ec44db42e86ccacf832794b1b9fefaa0";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url).then((res) => res.json());
+};
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    weather,
+    speed,
+  };
+};
+
+const getFormattedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+};
