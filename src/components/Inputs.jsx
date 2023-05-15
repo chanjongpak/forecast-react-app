7fd7b85b7c79651e323f78bdeb26a16c9f9476bb
chanjongpak <chanjong.pak@gmail.com>
2023-05-15 17:36:57 -0400

    add input handlers

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
-function Inputs() {
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState("");
+
+  const handleSearchButton = () => {
+    if (city !== "") setQuery({ q: city });
+  };
+
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           className="text-xl font-light p-2 w-full shadow-xl capitalize placeholder:lowercase"
           placeholder="search for a city"
         />
-        <UilSearch size={25} className="text-white cursor-pointer" />
-        <UilLocationPoint size={25} className="text-white cursor-pointer" />
+        <UilSearch
+          size={25}
+          onClick={handleSearchButton}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <UilLocationPoint
+          size={25}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
       </div>
 
       <div className="flex flex-row w-1/4 items-center justify-center">
-        <button name="metric" className="text-xl text-white font-light">
+        <button
+          name="metric"
+          className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={() => setUnits("metric")}
+        >
           °C
         </button>
         <p className="text-xl text-white mx-1">|</p>
-        <button name="imperical" className="text-xl text-white font-light">
+        <button
+          name="empirical"
+          className="text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={() => setUnits("empirical")}
+        >
           °F
         </button>
       </div>
